c27da7fdfa84c0a0710469306b17dc3c8326824b
Anurag Dwivedi <40738395+noobcod3r@users.noreply.github.com>
2020-08-28 23:48:25 +0530

    Add files via upload

---
+++ LIS.cpp
@@ -0,0 +1,64 @@
+// Book Shop :: https://cses.fi/problemset/task/1158/
+
+#include<bits/stdc++.h>
+#define fr(i,c,b) for(int i=c;i<b;i++)
+#define frl(i,c,b) for(ll i=c;i<b;i++)
+#define si(x) int x; scanf("%d", &x)
+#define sll(x) ll x; scanf("%lld", &x)
+#define pi(x) printf("%d", x)
+#define pll(x) printf("%lld", x)
+#define nl printf("\n")
+#define watch(x) cout << (#x) << " is " << (x) << endl
+#define MAX 100001
+#define M 1000000007
+#define pb push_back
+#define INF 10000001
+
+typedef long long int ll;
+
+using namespace std;
+
+int main()
+{
+   //freopen("input.txt","r",stdin);
+   sll(n); ll a[n]; fr(i,0,n)   cin>>a[i];
+   
+  
+   ll index[n+1]={0};
+
+    // index [i] = index of the smallest number in the LIS of length i 
+    // if A = 1,2,3     then index[1] = 1 [as the 1 is the smallest number in all the LIS of legnth 1]
+
+
+   index[0]=a[0];
+
+    ll len=1;
+   fr(i,1,n)
+   {
+       if(a[i]<index[0])    index[0]=a[i];
+
+       else if(a[i]>index[len-1]) index[len]=a[i] , len++;
+
+       else
+       {
+            ll l=-1,r=len-1;
+            while(r- l > 1)
+            {
+                ll mid=l+(r-l)/2;
+
+                if(a[i]>index[mid])
+                {
+                    l=mid;
+                }
+                else
+                    r=mid;
+            }
+            index[r]=a[i];
+       }
+
+
+   }
+   
+   //fr(i,0,n+1)  watch(index[i]);
+    cout<<len;
+}
